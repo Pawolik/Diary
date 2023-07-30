c2818c25917e24b68edca489b9e1c75f142bf91d
Paweł Jacewicz <pjacewicz474@gmail.com>
2023-07-30 16:08:03 +0200

    Moving connection string from App.config to user settings

--- Diary/ApplicationDbContext.cs
+++ Diary/ApplicationDbContext.cs
@@ -1,16 +1,30 @@
 using Diary.Models.Configurations;
 using Diary.Models.Domains;
 using System;
+using System.Configuration.Provider;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Diary
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext()
-            : base("name=ApplicationDbContext")
+            : base(GetConnectionStringFromSettings())
+        {            
+        }
+
+        private static string GetConnectionStringFromSettings()
         {
+            string serverAddress = Properties.Settings.Default.serverAddress;
+            string serverName = Properties.Settings.Default.serverName;
+            string databaseName = Properties.Settings.Default.databaseName;
+            string username = Properties.Settings.Default.username;
+            string password = Properties.Settings.Default.password;
+
+            return $"Server={serverAddress}\\{serverName};Database={databaseName};User Id={username};Password={password};";
         }
 
         public DbSet<Student> Students { get; set; }
--- Diary/Properties/Settings.Designer.cs
+++ Diary/Properties/Settings.Designer.cs
@@ -8,22 +8,90 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Diary.Properties
-{
-
-
+namespace Diary.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("(Local)")]
+        public string serverAddress {
+            get {
+                return ((string)(this["serverAddress"]));
+            }
+            set {
+                this["serverAddress"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Diary")]
+        public string databaseName {
+            get {
+                return ((string)(this["databaseName"]));
+            }
+            set {
+                this["databaseName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("DiaryDb")]
+        public string username {
+            get {
+                return ((string)(this["username"]));
+            }
+            set {
+                this["username"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("123")]
+        public string password {
+            get {
+                return ((string)(this["password"]));
+            }
+            set {
+                this["password"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("SQLEXPRESS")]
+        public string serverName {
+            get {
+                return ((string)(this["serverName"]));
+            }
+            set {
+                this["serverName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("System.Data.SqlClient")]
+        public string providerName {
+            get {
+                return ((string)(this["providerName"]));
+            }
+            set {
+                this["providerName"] = value;
             }
         }
     }
